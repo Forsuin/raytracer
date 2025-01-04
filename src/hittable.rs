202e9f2c4a2687492c5db9d46cd3a0b7a4b5c5e6
Ian Laidlaw <ian@kaboodles.com>
2025-01-03 23:53:40 -0500

    Added front/back faces

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,10 +1,25 @@
 use crate::ray::Ray;
 use crate::vec3::Vec3;
 
-pub(crate) struct HitRecord {
+pub struct HitRecord {
     pub point: Vec3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_vector: Vec3) {
+        // outward_vector assumed to have unit length
+
+        self.front_face = ray.direction.dot(&outward_vector) < 0.;
+        self.normal = if self.front_face {
+            outward_vector
+        }
+        else {
+            -outward_vector
+        };
+    }
 }
 
 pub trait Hittable {
