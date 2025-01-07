cd2ab2e93e789b47ec75dd791472cd9f3bc75a80
Ian Laidlaw <ilaidlaw@radford.edu>
2025-01-06 20:48:49 -0500

    Added HittableList

--- src/hittable.rs
+++ src/hittable.rs
@@ -25,3 +25,34 @@ impl HitRecord {
 pub trait Hittable {
     fn hit(&self, ray: &Ray, tmin: f64, tmax: f64) -> Option<HitRecord>;
 }
+
+pub struct HittableList {
+    pub objects: Vec<Box<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn add(&mut self, object: impl Hittable + 'static) {
+        self.objects.push(Box::new(object));
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, tmin: f64, tmax: f64) -> Option<HitRecord> {
+        let mut closest_so_far = tmax;
+        let mut hit_anything = None;
+
+        for object in self.objects.iter() {
+            if let Some(hit) = object.hit(ray, tmin, closest_so_far) {
+                closest_so_far = hit.t;
+                hit_anything = Some(hit);
+            }
+        }
+
+        hit_anything
+    }
+}
+
