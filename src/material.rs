966ddba5ee4da70c2b4ccdd996c4ca0256fbb535
Ian Laidlaw <ilaidlaw@radford.edu>
2025-01-08 16:35:41 -0500

    Added dialetric materials and light scattering for metals

--- src/material.rs
+++ src/material.rs
@@ -1,3 +1,4 @@
+use rand::random;
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
 use crate::vec3::{Color, Vec3};
@@ -38,12 +39,14 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
         Metal {
             albedo,
+            fuzz: if fuzz < 1. { fuzz } else { 1. },
         }
     }
 }
@@ -51,8 +54,61 @@ impl Metal {
 impl Material for Metal {
     fn scatter(&self, ray: &Ray, hit: &HitRecord) -> Option<(Ray, Color)> {
         let reflected = reflect(&ray.direction, &hit.normal);
+        let reflected = reflected.unit_vector() + (self.fuzz * random_unit_vector());
         let scattered = Ray::new(hit.point, reflected);
         let attenuation = self.albedo;
+
+        if scattered.direction.dot(&hit.normal) < 0. {
+            return None;
+        }
+
+        Some((scattered, attenuation))
+    }
+}
+
+pub struct Dialetric {
+    refraction_index: f64,
+}
+
+impl Dialetric {
+    pub fn new(refraction_index: f64) -> Self {
+        Self {
+            refraction_index,
+        }
+    }
+
+    fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
+        let r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
+        let r0 = r0 * r0;
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
+}
+
+impl Material for Dialetric {
+    fn scatter(&self, ray: &Ray, hit: &HitRecord) -> Option<(Ray, Color)> {
+        let attenuation = Color::ONE;
+        let ri = if hit.front_face {
+            1.0 / self.refraction_index
+        } else {
+            self.refraction_index
+        };
+
+        let unit_direction = ray.direction.unit_vector();
+        let cos_theta = f64::min((-unit_direction).dot(&hit.normal), 1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let cannot_reflect = ri * sin_theta > 1.0;
+        let direction: Vec3;
+
+        if (cannot_reflect || Self::reflectance(cos_theta, ri) > random::<f64>()) {
+            direction = reflect(&unit_direction, &hit.normal);
+        }
+        else {
+            direction = refract(&unit_direction, &hit.normal, ri);
+        }
+
+
+        let scattered = Ray::new(hit.point, direction);
 
         Some((scattered, attenuation))
     }
@@ -81,4 +137,12 @@ fn random_on_hemisphere(normal: &Vec3) -> Vec3 {
 
 fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
     v - 2.0 * v.dot(n) * n
+}
+
+
+fn refract(uv: &Vec3, n: &Vec3, etai_over_etat: f64) -> Vec3 {
+    let cos_theta = f64::min(-uv.dot(n), 1.0);
+    let r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    let r_out_parallel = -f64::abs(1.0 - r_out_perp.length_squared()).sqrt() * n;
+    r_out_perp + r_out_parallel
 }
